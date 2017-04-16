375a85c8be0a60ffa88580f7f97dd90d12e54ed2
Alphy Joseph <lizjoseph18@rocketmail.com>
2017-04-16 12:32:48 +0530

    fixes to finding nth node from end

--- nfromend.c
+++ nfromend.c
@@ -7,36 +7,48 @@ struct node
 };
 void push(struct node **head,int data)
 {
-	struct node * next;
+	struct node* next;
 	next=(struct node*)malloc(sizeof(struct node));
 	next->info=data;
-	next->link=head;
+	next->link=*head;
 	*head=next;
 }
-void nfromend(struct node **head,int n)
+void nfromend(struct node *head,int n)
 {
-	struct node *hd,*ref;
-	*ref=*hd;
+	struct node *hd=head;
+	struct node *ref=head;
 	int count=0;
-	while(count<n)
+	if(head!=NULL)
 	{
-		ref=ref->next;
-		count++;
-	}
-	while(ref!=NULL)
-	{
-		ref=ref->next;
-		hd=hd->next;
-	}
+		while(count<n)
+		{
+			if(ref==NULL)
+			{
+				printf("The value of n is greater than the number of nodes");
+			}
+			ref=ref->link;
+			count++;
+		}
+
+		while(ref!=NULL)
+		{
+			ref=ref->link;
+			hd=hd->link;
+		}
 	printf("%d\n",head->info );
+    }
+    else
+    {
+    	printf("Linked List does not exist");
+    }
 }
 int main()
 {
-	struct node **head=NULL;
+	struct node *head=NULL;
 	push(&head,4);
 	push(&head,15);
 	push(&head,25);
 	push(&head,42);
-	nfromend(&head,3);
+	nfromend(head,3);
 	return 0;
 }
