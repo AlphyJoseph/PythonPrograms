3306fbfb424211060063dd00ac11ec531baedbe5
Alphy Joseph <lizjoseph18@rocketmail.com>
2017-04-17 22:35:05 +0530

    unique characters in a string

---
+++ unique.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string.h>
+#include<stdlib.h>
+using namespace std;
+//#include<process.h>
+void uniqueChar(char str[30])
+{
+	int char_set[128];
+	/*if (strlen(str)>128)
+	{
+		cout<<"Characters are not unique"<<endl;
+		exit(1);
+	}*/
+	for(int i=0;i<strlen(str);i++)
+	{
+		int val=str[i];
+		if(char_set[val])
+		{	cout<<"Characters are not unique"<<endl;
+			exit(1);
+		}
+		char_set[val]=1;
+	}
+	cout<<"All Characters in the string are unique"<<endl;
+}
+int main()
+{
+	cout<<"Enter String"<<endl;
+	char str1[30];
+	cin>>str1;
+	uniqueChar(str1);
+	return 0;
+
+}
